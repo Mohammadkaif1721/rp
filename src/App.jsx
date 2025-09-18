b0d6d2a7b339c30648bffb9422b0cee8a7d4f181
Mohammadkaif1721 <kaif66687@gmail.com>
2025-09-18 22:35:25 +0530

    Initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import './global-mobile.css'
-import Navbar from './components/navbar/Navbar'
+import Navbar from './components/Navbar/Navbar'
 import Hero from './components/Hero/Hero'
 import About from './components/About/About'
 import Skills from './components/Skills/Skills'
